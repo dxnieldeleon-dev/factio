5237780f8d57f53eb6becae1dc84eb1095347107
dxnieldeleon-dev <danielmissaeldeleon@gmail.com>
2026-07-21 16:29:00 -0600

    Implement Product entity class
    Added Product entity with constructor for initialization.

--- supabase/functions/shared/domain/Product.ts
+++ supabase/functions/shared/domain/Product.ts
@@ -4,3 +4,31 @@
  * TODO:
  * Implement domain model.
  */
+/**
+ * ------------------------------------------------------------------------
+ * Product Entity
+ * ------------------------------------------------------------------------
+ */
+
+export class Product {
+  constructor(
+    private readonly id: string,
+    private readonly companyId: string,
+
+    private readonly code: string,
+
+    private readonly description: string,
+
+    private readonly unitCode: string,
+    private readonly productCode: string,
+
+    private readonly unitPrice: number,
+
+    private readonly taxRate: number,
+
+    private readonly isActive: boolean,
+
+    private readonly createdAt: Date,
+    private readonly updatedAt: Date
+  ) {}
+}
