22de48846f50d90981941959a9f2f39faba3d06b
dxnieldeleon-dev <danielmissaeldeleon@gmail.com>
2026-07-21 17:48:40 -0600

    Change types for id, companyId, rfc, and email

--- supabase/functions/shared/domain/Client.ts
+++ supabase/functions/shared/domain/Client.ts
@@ -12,19 +12,25 @@
 
 export class Client {
   constructor(
-    private readonly id: string,
-    private readonly companyId: string,
+    private readonly id: UUID,
+    private readonly companyId: UUID,
 
-    private readonly rfc: string,
+    private readonly rfc: RFC,
     private readonly name: string,
 
     private readonly taxRegime: string,
     private readonly postalCode: string,
     private readonly cfdiUse: string,
 
-    private readonly email: string | null,
+    private readonly email: Email | null,
 
     private readonly createdAt: Date,
     private readonly updatedAt: Date
+    ts
+import {
+  UUID,
+  RFC,
+  Email,
+} from "../value-objects";
   ) {}
 }
