8b29d45e47983217ffef09f8e4e8911dc8dec3ff
dxnieldeleon-dev <danielmissaeldeleon@gmail.com>
2026-07-21 16:28:28 -0600

    Create User entity with constructor and properties
    Implemented the User entity with relevant properties.

--- supabase/functions/shared/domain/User.ts
+++ supabase/functions/shared/domain/User.ts
@@ -1,6 +1,23 @@
 /**
- * Domain Model
- *
- * TODO:
- * Implement domain model.
+ * ------------------------------------------------------------------------
+ * User Entity
+ * ------------------------------------------------------------------------
  */
+
+export class User {
+  constructor(
+    private readonly id: string,
+    private readonly companyId: string,
+
+    private readonly email: string,
+    private readonly fullName: string,
+
+    private readonly role: string,
+    private readonly status: string,
+
+    private readonly lastLoginAt: Date | null,
+
+    private readonly createdAt: Date,
+    private readonly updatedAt: Date
+  ) {}
+}
