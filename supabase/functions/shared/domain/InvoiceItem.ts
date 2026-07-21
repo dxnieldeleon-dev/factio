aaef09d3f2331d5bc1726fd2fad23b66c4c04a66
dxnieldeleon-dev <danielmissaeldeleon@gmail.com>
2026-07-21 16:29:27 -0600

    Implement InvoiceItem class structure
    Define the InvoiceItem class with its properties.

--- supabase/functions/shared/domain/InvoiceItem.ts
+++ supabase/functions/shared/domain/InvoiceItem.ts
@@ -4,3 +4,30 @@
  * TODO:
  * Implement domain model.
  */
+/**
+ * ------------------------------------------------------------------------
+ * Invoice Item Entity
+ * ------------------------------------------------------------------------
+ */
+
+export class InvoiceItem {
+  constructor(
+    private readonly id: string,
+
+    private readonly invoiceId: string,
+
+    private readonly productId: string | null,
+
+    private readonly description: string,
+
+    private readonly quantity: number,
+
+    private readonly unitPrice: number,
+
+    private readonly discount: number,
+
+    private readonly taxes: number,
+
+    private readonly total: number
+  ) {}
+}
