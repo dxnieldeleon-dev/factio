6a16675d2963ccf9c178c70d018ada730a3eb89d
dxnieldeleon-dev <danielmissaeldeleon@gmail.com>
2026-07-21 16:29:58 -0600

    Implement Subscription class in Subscription.ts
    Added Subscription class with properties for subscription details.

--- supabase/functions/shared/domain/Subscription.ts
+++ supabase/functions/shared/domain/Subscription.ts
@@ -4,3 +4,32 @@
  * TODO:
  * Implement domain model.
  */
+/**
+ * ------------------------------------------------------------------------
+ * Subscription Entity
+ * ------------------------------------------------------------------------
+ */
+
+export class Subscription {
+  constructor(
+    private readonly id: string,
+
+    private readonly companyId: string,
+
+    private readonly stripeCustomerId: string,
+
+    private readonly stripeSubscriptionId: string,
+
+    private readonly plan: string,
+
+    private readonly status: string,
+
+    private readonly startedAt: Date,
+
+    private readonly expiresAt: Date | null,
+
+    private readonly createdAt: Date,
+
+    private readonly updatedAt: Date
+  ) {}
+}
