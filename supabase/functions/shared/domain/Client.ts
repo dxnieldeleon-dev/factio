4adaf634a9c3f4e88d6948d27516fea022ca1af0
dxnieldeleon-dev <danielmissaeldeleon@gmail.com>
2026-07-21 16:28:46 -0600

    Update Client.ts

--- supabase/functions/shared/domain/Client.ts
+++ supabase/functions/shared/domain/Client.ts
@@ -4,3 +4,27 @@
  * TODO:
  * Implement domain model.
  */
+/**
+ * ------------------------------------------------------------------------
+ * Client Entity
+ * ------------------------------------------------------------------------
+ */
+
+export class Client {
+  constructor(
+    private readonly id: string,
+    private readonly companyId: string,
+
+    private readonly rfc: string,
+    private readonly name: string,
+
+    private readonly taxRegime: string,
+    private readonly postalCode: string,
+    private readonly cfdiUse: string,
+
+    private readonly email: string | null,
+
+    private readonly createdAt: Date,
+    private readonly updatedAt: Date
+  ) {}
+}
