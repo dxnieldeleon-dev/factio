1e362d8ba7f6ffec1f8bfbffb57f88a1ee678b37
dxnieldeleon-dev <danielmissaeldeleon@gmail.com>
2026-07-21 16:29:42 -0600

    Implement Wallet class in domain model
    Added Wallet class with properties and constructor.

--- supabase/functions/shared/domain/Wallet.ts
+++ supabase/functions/shared/domain/Wallet.ts
@@ -4,3 +4,24 @@
  * TODO:
  * Implement domain model.
  */
+/**
+ * ------------------------------------------------------------------------
+ * Wallet Entity
+ * ------------------------------------------------------------------------
+ */
+
+export class Wallet {
+  constructor(
+    private readonly id: string,
+
+    private readonly companyId: string,
+
+    private readonly availableStamps: number,
+
+    private readonly usedStamps: number,
+
+    private readonly createdAt: Date,
+
+    private readonly updatedAt: Date
+  ) {}
+}
