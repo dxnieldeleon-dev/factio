a76cf20e9bea7ef2ba918638b0451bc82c336d9e
dxnieldeleon-dev <danielmissaeldeleon@gmail.com>
2026-07-21 16:30:12 -0600

    Export domain models from index.ts

--- supabase/functions/shared/domain/index.ts
+++ supabase/functions/shared/domain/index.ts
@@ -4,3 +4,11 @@
  * TODO:
  * Implement domain model.
  */
+export * from "./Company";
+export * from "./User";
+export * from "./Client";
+export * from "./Product";
+export * from "./Invoice";
+export * from "./InvoiceItem";
+export * from "./Wallet";
+export * from "./Subscription";
